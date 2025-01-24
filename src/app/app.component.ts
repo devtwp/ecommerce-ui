9adb478e4fd404d97e0cf3092f79a821a607e595
devtwp <devtwp1@gmail.com>
2025-01-23 21:07:12 -0300

    update test images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent  implements OnInit {
   products = [
-    { id: 1, name: 'Producto 1', description: 'Descripción 1', price: 100, image: 'https://via.placeholder.com/150' },
-    { id: 2, name: 'Producto 2', description: 'Descripción 2', price: 200, image: 'https://via.placeholder.com/150' },
-    { id: 3, name: 'Producto 3', description: 'Descripción 3', price: 300, image: 'https://via.placeholder.com/150' },
+    { id: 1, name: 'Producto 1', description: 'Descripción 1', price: 100, image: 'https://media.istockphoto.com/id/1309352410/es/foto/hamburguesa-con-queso-con-tomate-y-lechuga-en-tabla-de-madera.jpg?s=612x612&w=0&k=20&c=HaSLXFFns4_IHfbvWY7_FX7tlccVjl0s0BrlqaLHOTE=' },
+    { id: 2, name: 'Producto 2', description: 'Descripción 2', price: 200, image: 'https://media.istockphoto.com/id/1448322070/es/foto/sabrosa-hamburguesa-fresca-en-mesa-de-madera.jpg?s=612x612&w=0&k=20&c=JrAb5GidOn0_mUEnsncdUQfmKwKtYC5p-JemGfpQN0w=' },
+    { id: 3, name: 'Producto 3', description: 'Descripción 3', price: 300, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7q9bvvrqXPiLQ7GgISVx925JoUeXMe0u35g&s' },
     // Agrega más productos según sea necesario (mínimo 20 para probar la paginación)
-    { id: 21, name: 'Producto 21', description: 'Descripción 21', price: 2100, image: 'https://via.placeholder.com/150' },
+    { id: 21, name: 'Producto 21', description: 'Descripción 21', price: 2100, image: 'https://s2.abcstatics.com/abc/sevilla/media/gurme/2024/09/19/s/receta-de-la-mejor-hamburguesa-del-mundo.jpg-khAC--1248x698@abc.jpg' },
   ];
 
   paginatedProducts: any[] = []; // Productos mostrados en la página actual
