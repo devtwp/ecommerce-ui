fda86ba7954f6f821a4f24433937026d4763a9af
devtwp1 <devtwp1@gmail.com>
2025-02-03 20:26:59 -0300

    Agrega header, footer, crea home y card component

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}" // Importante para que Tailwind escanee los archivos de Angular
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
