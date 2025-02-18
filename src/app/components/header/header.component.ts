44215cbe07f26029d5bbaa0db9fd16507d79fc01
Fermin Robledo <fermin4robledo@gmail.com>
2025-02-18 20:08:35 -0300

    Arregla bug del nav

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,5 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  
+
 }
